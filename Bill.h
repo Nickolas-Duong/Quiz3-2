ef9d4633ba960245ea190057c23cbb73ac244488
Nickolas Duong <nduong@cpp.edu>
2022-10-21 19:07:04 -0700

    Add basic header file for Bill.h

--- Bill.h
+++ Bill.h
@@ -1,12 +1,27 @@
 //Bill.h prepares the struct for the program to use
 
 //Header file for project
+#ifndef BILL_H
+#define BILL_H
+
 //Libraries used
+#include <stdio.h>
+#include <stddef.h>
 
 //pointer struct for restaurant items
+typedef struct
+{
     //menu number
-    //name
-    //cost
-//Pointer info to link ptrs
+    int menuNum;
 
+    //name
+    char *menuName;
+
+    //cost
+    double menuCost;
+}Menu, *PtrToMenu;
+//Pointer info to link ptrs
+typedef const Menu *PtrToConstMenu;
 //Function Definitions used in the 
+
+#endif
