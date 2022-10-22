43ba8b1d601f1bd51cc012ebcd3d138f679f0e9c
Nickolas Duong <nduong@cpp.edu>
2022-10-21 21:04:48 -0700

    Add code to check if correct arguments supplied

--- main.c
+++ main.c
@@ -8,72 +8,80 @@
 //main function - argc and argv[]
 void main(int argc, char * argv[])
 {
-    //function prototypes
-    PtrToMenu searchMenuByNumber(const Menu table[], int sizeTable, int numberToFind);
-
-    //extern table
-    extern Menu MenuTable[];
-
-    //extern table size
-    extern const int MenuTableEntries;
-
-    //random generate how many items in array
-    time_t t;
-    PtrToMenu matchPtr;
-    int maxOrder, count;
-    double cost = 0;
-    double tax, tip;
-
-    //Randomize random
-    srand((unsigned) time(&t));
-    
-    //set max order size
-    maxOrder = (rand() % 4) + 1;
-    
-    printf("ORDER RECIEPT\n\n");
-    
-    //generate max order
-    for(int i = 0; i < maxOrder; i++)
+    if(argc == 2)
     {
-        //get menu item
-        matchPtr = searchMenuByNumber(MenuTable, MenuTableEntries, (rand() % 4)+1);
+        //function prototypes
+        PtrToMenu searchMenuByNumber(const Menu table[], int sizeTable, int numberToFind);
 
-        //if found
-        if (matchPtr != NULL)
-        {
-            //print item information
-            printf("Menu number %d\n", matchPtr->menuNum);
-            printf("%s\n", matchPtr->menuName);
-            printf("Item Cost: $%f\n\n", matchPtr->menuCost);
+        //extern table
+        extern Menu MenuTable[];
 
-            //increase cost
-            cost = cost + matchPtr->menuCost;
-            count++;
-        }
-        //if not found
-        else
+        //extern table size
+        extern const int MenuTableEntries;
+
+        //random generate how many items in array
+        time_t t;
+        PtrToMenu matchPtr;
+        int maxOrder, count;
+        double cost = 0;
+        double tax, tip;
+
+        //Randomize random
+        srand((unsigned) time(&t));
+        
+        //set max order size
+        maxOrder = (rand() % 4) + 1;
+        
+        printf("ORDER RECIEPT\n\n");
+
+        //generate max order
+        for(int i = 0; i < maxOrder; i++)
         {
-            printf("Menu number not found\n");
+            //get menu item
+            matchPtr = searchMenuByNumber(MenuTable, MenuTableEntries, (rand() % 4)+1);
+
+            //if found
+            if (matchPtr != NULL)
+            {
+                //print item information
+                printf("Menu number %d\n", matchPtr->menuNum);
+                printf("%s\n", matchPtr->menuName);
+                printf("Item Cost: $%f\n\n", matchPtr->menuCost);
+
+                //increase cost
+                cost = cost + matchPtr->menuCost;
+                count++;
+            }
+            //if not found
+            else
+            {
+                printf("Menu number not found\n");
+            }
         }
+
+        printf("Total Items: %d\n", count);
+        printf("Subtotal: %f\n", cost);
+        
+        //declare local variable for tax percent based on inputs -> if no input assume 0
+        tax = atof(argv[1]);
+        
+        //declare local variable for tip percent based on inputs -> if no input assume 0
+        tip = atof(argv[2]);
+
+        //print tax percent and total tax
+        printf("Tax Percent: %f\nTax: $%f\n", tax, tax*cost);
+
+        //print tip percent and total tip
+        printf("Tip Percent: %f\nTip: $%f\n", tip, tip*cost);
+        
+        //print final bill cost
+        printf("Total: $%f\n", cost+(tax*cost)+(tip*cost));
+
+        EXIT_SUCCESS;
     }
-
-    printf("Total Items: %d\n", count);
-    printf("Subtotal: %f\n", cost);
-    
-    //declare local variable for tax percent based on inputs -> if no input assume 0
-    tax = atof(argv[1]);
-    
-    //declare local variable for tip percent based on inputs -> if no input assume 0
-    tip = atof(argv[2]);
-
-    //print tax percent and total tax
-    printf("Tax Percent: %f\nTax: $%f\n", tax, tax*cost);
-
-    //print tip percent and total tip
-    printf("Tip Percent: %f\nTip: $%f\n", tip, tip*cost);
-    
-    //print final bill cost
-    printf("Total: $%f\n", cost+(tax*cost)+(tip*cost));
-
-    EXIT_SUCCESS;
+    else if (argc < 2 || argc > 2)
+    {
+        printf("Incorrect Argument count\nArguments Needed: 2\nUsed as Tax and Tip\n");
+        EXIT_FAILURE;
+    }
 }
