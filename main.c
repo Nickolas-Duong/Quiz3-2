0110212d4cc43ab1c6b2b42e78768c98486ff4eb
Nickolas Duong <nduong@cpp.edu>
2022-10-21 20:18:44 -0700

    Add main function and randomize menu item

--- main.c
+++ main.c
@@ -1,27 +1,46 @@
 //main.c hosts the code to run the program, pulling from Table.c, Bill.h and BillFunctions.c in order to print a reciept for the user to view
 //When run, the user also inputs two arguments: Tax and Tip percentages.
+#include <time.h>
+#include <string.h>
+#include <stdlib.h>
+#include "Bill.h"
 
 //main function - argc and argv[]
-//function prototypes
+void main()
+{
+    //function prototypes
+    PtrToMenu searchMenuByNumber(const Menu table[], int sizeTable, int numberToFind);
 
-//extern table
-//extern table size
+    //extern table
+    extern Menu MenuTable[];
 
-//random generate how many items in array
-//declare local pointer array for menu items
-//declare local variable for tax percent based on inputs -> if no input assume 0
-//declare local variable for tip percent based on inputs -> if no input assume 0
+    //extern table size
+    extern const int MenuTableEntries;
 
-//for loop for array - loop through max size of pointer array
-    //return matching pointer each loop, based on randomizer function
+    //random generate how many items in array
+    time_t t;
+    PtrToMenu matchPtr;
 
-//for loop for array - loop through and print all menu items in array
-    //Print information for each menu item
+    srand((unsigned) time(&t));
 
-//print tax percent and total tax
+    matchPtr = searchMenuByNumber(MenuTable, MenuTableEntries, (rand() % 4)+1);
+ 
+    if (matchPtr != NULL)
+    {
+        printf("Menu number %d\n", matchPtr->menuNum);
+    }
+    else
+    {
+        printf("Menu number not found\n");
+    }
+    //declare local variable for tax percent based on inputs -> if no input assume 0
+    //declare local variable for tip percent based on inputs -> if no input assume 0
 
-//print tip percent and total tip
+    //print tax percent and total tax
 
-//print final bill cost
+    //print tip percent and total tip
 
-//EXIT_SUCCESS
+    //print final bill cost
+    
+    EXIT_SUCCESS;
+}
